adfe3e1a1d14992b9e7a1eef6d776e20b37af66f
Ashutosh Kumar <itsray650@gmail.com>
2025-12-02 18:02:15 +0530

    render api

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://book-saloon-backend.vercel.app/api',
+    baseURL: 'https://booksaloon-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
